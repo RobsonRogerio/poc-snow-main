011cb380930620ce9a6073fca264d3df2b5c45b3
RobsonRogerio <robson.rogerio@gmail.com>
2025-10-24 10:00:47 -0300

    novas tabelas bronze adicionadas

--- sql/DDL/bronze/bronze_orders_details.sql
+++ sql/DDL/bronze/bronze_orders_details.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS bronze_orders (
+CREATE TABLE IF NOT EXISTS bronze_orders_details (
     raw VARIANT,           -- JSON bruto como $1
     filename STRING,       -- Nome do arquivo
     created_at TIMESTAMP   -- Timestamp de carga
