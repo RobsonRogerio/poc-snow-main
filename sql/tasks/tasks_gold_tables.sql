32033fe7f45e92f6c6a6ef672a14b3189fb0967f
RobsonRogerio <robson.rogerio@gmail.com>
2025-10-25 17:24:06 -0300

    comentário para gerar pr

--- sql/tasks/tasks_gold_tables.sql
+++ sql/tasks/tasks_gold_tables.sql
@@ -1,4 +1,4 @@
--- Task 1
+-- Task 1: Criação da task para carregar a tabela fato de pedidos na camada gold
 CREATE OR REPLACE TASK task_load_gold_fact_orders
   WAREHOUSE = COMPUTE_WH
   AFTER task_load_silver_products
